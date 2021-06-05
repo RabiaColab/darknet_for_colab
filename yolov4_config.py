7cd0c92263fe06d417c223e2ecf00964e98e0049
RabiaColab <83351751+RabiaColab@users.noreply.github.com>
2021-06-06 01:25:31 +0900

    Update yolov4_config.py

--- yolov4_config.py
+++ yolov4_config.py
@@ -11,15 +11,15 @@ and cfg/yolov4_custom_test.cfg
 
 """
 
-classes=2
+classes=6
 max_batches=8000
 batch=64
 subdivisions=16
 width=224
 height=224
-channels=3
+channels=1
 momentum=0.949
 decay=0.0005
-learning_rate=0.0001
+learning_rate=0.000005
 steps= (6400,7200)
 scales=(0.1,0.1)
